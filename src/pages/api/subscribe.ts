2f3a8aa8ce91b4e5a786f31614008ba7bf51c3d7
Jarbas Santana <jarbasfilho29@gmail.com>
2022-01-04 01:05:27 -0300

    checkout session

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,5 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../services/stripe'
 
-export default async (req: NextApiRequest, res: NextApiResponse){
-    
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        const checkoutSession = await stripe.checkout.sessions.create({
+            payment_method_types
+        })
+        return
+    }
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
 }
